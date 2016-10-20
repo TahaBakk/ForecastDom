c724c81c98abe078e66fc06f706e82fcc6476ab6
Taha <x3727349s@iespoblenou.org>
2016-10-20 16:43:38 +0200

    S'imprimeixen les dades, hem pasat de mps a mph

--- src/Main.java
+++ src/Main.java
@@ -28,19 +28,33 @@ public class Main {
             Document db = dbBuilder.parse(inputFile);
             System.out.println("Root element : " + db.getDocumentElement().getNodeName());
 
+            //Aquet NodeList el creem per extrure nomes el nom de la localització que es troba en l'apartat location
             NodeList lista = db.getElementsByTagName( "location" );
 
+            //Aqui guardem el nom de la localitzqació en una varible
             Element namelista = (Element) lista.item(0);
-            System.out.println("El lloc és: "+namelista.getElementsByTagName("name").item(0).getTextContent());
+            String nomLocalitzacio= namelista.getElementsByTagName("name").item(0).getTextContent();
 
+            //Aquet NodeList el creem per extrure totes les dades que te l'apartat temps
             NodeList list = db.getElementsByTagName("time");
 
+            //Recorrem tot el llistat y l'imprimim amb aquet for
             for (int i = 0; i < list.getLength(); i++) {
 
                 Element eLista = (Element) list.item(i);
 
-                System.out.println("La temperatura és: "+eLista.getElementsByTagName("temperature").item(0).getAttributes().getNamedItem("value").getTextContent()+"graus celsius");
+                //Això es per a saber la velocitat en mps i en mph
+                double velocitat =Double.parseDouble(eLista.getElementsByTagName("windSpeed").item(0).getAttributes().getNamedItem("mps").getTextContent());
+                double velocitatmph = velocitat*3.6;
+
+                //Aqui imprimim le dades
+                System.out.println("La localització és: "+ nomLocalitzacio);
+                System.out.println("La temperatura és: "+eLista.getElementsByTagName("temperature").item(0).getAttributes().getNamedItem("value").getTextContent()+" "+eLista.getElementsByTagName("temperature").item(0).getAttributes().getNamedItem("unit").getTextContent());
                 System.out.println("El temps és: "+eLista.getElementsByTagName("symbol").item(0).getAttributes().getNamedItem("name").getTextContent());
+                System.out.println("La velocitat en mps és: "+velocitat+" mps");
+                System.out.println("La velocitat en mph és: "+velocitatmph+" mph");
+                System.out.println("");
+                System.out.println("");
 
             }
 
